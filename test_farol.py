27db3cf35234bf4c70cdf968b7250b6415fa4de0
nunoalves <nmgba@iscte-iul.pt>
2025-12-09 13:06:18 +0000

    Second commit

--- test_farol.py
+++ test_farol.py
@@ -1,5 +1,16 @@
 from Simulator import Simulator
-print("hello")
-sim = Simulator.cria("world_farol.txt")
-resultado = sim.executa()
-print(resultado)
+from utils import evoluir
+from Sensor import SensorObservacao, SensorLivre
+
+FICHEIRO = "world_farol.txt"
+
+print("--- TREINO ---")
+melhor = evoluir(FICHEIRO, populacao=30, geracoes=100, max_passos=20)
+
+print("\n--- DEMONSTRAÇÃO ---")
+sim = Simulator.cria(FICHEIRO)
+melhor.sensores = []
+melhor.instala(SensorObservacao(sim.world, 0))
+melhor.instala(SensorLivre(sim.world, 0))
+sim.agents[0] = melhor
+sim.executa(max_passos=50, render=True, delay=0.6)
